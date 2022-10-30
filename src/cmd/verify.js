0914cdcaf5ba111b134e99ab7f53c06e2a641666
ppoliani <ppoliani@gmail.com>
2022-10-30 12:59:14 +0200

    feat: add code to add roles

--- src/cmd/verify.js
+++ src/cmd/verify.js
@@ -1,5 +1,5 @@
 require('dotenv').config();
-const {SlashCommandBuilder} = require('discord.js')
+const {SlashCommandBuilder, GuildMemberManager, Role} = require('discord.js')
 
 module.exports = {
 	data: new SlashCommandBuilder()
@@ -10,6 +10,15 @@ module.exports = {
 		await interaction.reply({
       content: 'Verify your pass https://ticketland.io/verify',
       ephemeral: true,
-    });
+    })
+
+    // TODO: this logic will be async and executed in some other service. 
+    // After user verified the pass with Ticketland he will be assigned a role
+    let role = interaction.member.guild.roles.cache.find(role => role.name === 'ticketland-pass');
+    if (role) { 
+      interaction.guild.members.cache
+      .get(interaction.member.id)
+      .roles.add(role)
+    }
 	},
 };
